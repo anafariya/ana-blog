a95c5b17a793080bfbe752d9d689ba122a16f48e
Ana Fariya <anafariya@gmail.com>
2024-02-22 12:37:21 +0530

    &apos added to banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ function Banner() {
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
       <div>
         <h1 className="text-7xl py-5 text-gray-800 lg:text-6xl ">
-          Ana's {' '}
+          Ana&apos;s {' '}
           <span className="underline decoration-4 decoration-red-900">Daily Blog</span>
         </h1>
         <h2 className="mt-2 lg:mt-0 text-xl lg:text-xl text-gray-700">
